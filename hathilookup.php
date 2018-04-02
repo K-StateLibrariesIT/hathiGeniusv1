dfa14139fa79576a19359b002a0a1788c4e844a1
K-State Libraries IT <37344000+K-StateLibrariesIT@users.noreply.github.com>
2018-04-02 15:13:06 -0500

    Update hathilookup.php
    A more current version of the php script that eliminates some of the wasteful calls with no data.

--- hathilookup.php
+++ hathilookup.php
@@ -12,7 +12,13 @@
 	ini_set("log_errors", True);
 	ini_set("error_log", "../klib/logs/hathigenius-errors.log");
 
-	$datasink=json_decode($_GET["datasink"]);
+	$datasink=$_GET["datasink"];
+	$checko=strlen($datasink);
+	$datasink=json_decode($datasink);
+	/*this area contains our proprietary tests that we use, in concert with rules on the server, to protect from misuse. The
+	tests migrate into numerical values for the $test1 and $test2 variables in the conditional below.
+	Omit those vars from the conditional in the absence of such testing in this block*/
+	if(($checko>3) && ($test1>0) && ($test2>0)) {
 	
 	foreach ($datasink as &$value) {
 		$query="https://catalog.hathitrust.org/api/volumes/brief/htid/".$value->article.".json";
@@ -36,5 +42,9 @@
 	}
 	
 	echo("var datareturn='".json_encode($datasink)."';");
+
+}
+	
+
 	
 ?>
