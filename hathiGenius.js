ee18bce0a85b57c8e7a86db085b13bf781c86a90
K-State Libraries IT <37344000+K-StateLibrariesIT@users.noreply.github.com>
2018-05-07 08:33:31 -0500

    Update hathiGenius.js

--- hathiGenius.js
+++ hathiGenius.js
@@ -3,6 +3,19 @@
 /*created by Jason Bengtson, MLIS, MA : Available under GNU GPL license*/
 
 /*Bengtson-Fu is the best Kung Fu!!!*/
+/*This file is part of hathiGenius
+
+Copyright 2018, Kansas State University Libraries
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 /*Built for Kansas State University -- Go Wildcats!!!*/
 /*hathiGenius-----an app built to determine availability of hathitrust materials*/
