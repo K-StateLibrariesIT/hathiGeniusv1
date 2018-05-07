efcb79f3c545f629156700968166eeca4a8475ab
K-State Libraries IT <37344000+K-StateLibrariesIT@users.noreply.github.com>
2018-05-07 08:34:13 -0500

    Update hathilookup.php

--- hathilookup.php
+++ hathilookup.php
@@ -2,6 +2,19 @@
 /*Bengtson-Fu 13 3 |\| ( ][ 5 () |\| - |= |_|!!!!!!*/
 
 /*created by Jason Bengtson, MLIS, MA : Available under GNU GPL license*/
+/*This file is part of hathiGenius
+
+Copyright 2018, Kansas State University Libraries
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 /*Bengtson-Fu is the best Kung Fu!!!*/
 
